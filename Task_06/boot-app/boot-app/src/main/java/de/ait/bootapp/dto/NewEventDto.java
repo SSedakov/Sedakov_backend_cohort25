d739adbae15b203c0d2713e83f484f921b71e412
SSedakov <sedakovsergey1@gmail.com>
2023-09-27 09:47:10 +0200

    ''

--- Task_06/boot-app/boot-app/src/main/java/de/ait/bootapp/dto/NewEventDto.java
+++ Task_06/boot-app/boot-app/src/main/java/de/ait/bootapp/dto/NewEventDto.java
@@ -6,6 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,7 +19,12 @@ import lombok.Setter;
 public class NewEventDto {
 
     @Schema(description = "Название события",example = "День рождения")
+    @Size(min = 5, max = 1000)
+    @NotBlank
+    @NotNull
     private String eventName;
+
+    @Pattern(regexp = "^(?:(?:19|20)\\d\\d)-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2\\d|3[0-1])$")
     @Schema(description = "Описание события",example = "День рождения у бабушки 30 сентября")
     private String title;
 }
